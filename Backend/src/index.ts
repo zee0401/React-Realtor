cb760ec8f9e7358f10c714e71a780d37714cb740
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-05-25 02:54:40 +0530

    port

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5000/",
+    origin: "http://localhost:5000",
     credentials: true,
   })
 );
